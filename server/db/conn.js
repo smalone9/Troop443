cd3475da93c304a10a5886ad4b0a0a8b2112c8ea
Samantha Malone <sammac99@gmail.com>
2022-01-11 14:46:21 -0700

    added conn.js to connect db

---
+++ server/db/conn.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require("mongodb");
+const Db = process.env.ATLAS_URI;
+const client = new MongoClient(Db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+var_db;
+
+module.exports = {
+  connectToServer: function (callback) {
+    client.connect(function (err, db) {
+      //verify db object
+      if (db) {
+        _db = db.db("troopDatabase");
+        console.log("Connected to MongoDB");
+      }
+      return callback(err);
+    });
+  },
+  getDb: function () {
+    return_db;
+  },
+};
