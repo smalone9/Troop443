3dc7f80878598922f828a7f4e6d59b74fcc98416
Samantha Malone <sammac99@gmail.com>
2022-01-12 17:08:11 -0700

    added table display for recordList

---
+++ client/src/components/recordList.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Record = (props) => (
+  <tr>
+    <td>{props.record.name}</td>
+    <td>{props.record.contact}</td>
+    <td>{props.record.level}</td>
+    <td>
+      <Link className="btn btn-link" to={`/edit/${props.record._id}`}>
+        Edit
+      </Link>
+      |
+      <button
+        className="btn btn-link"
+        onClick={() => {
+          props.deleteRecord(props.record._id);
+        }}
+      >
+        Delete
+      </button>
+    </td>
+  </tr>
+);
+
+export default function RecordList() {
+  const [records, setRecords] = useState([]);
+
+  //method fetches records from db
+  useEffect(() => {
+    async function getRecords() {
+      const response = await fetch(`http://localhost/5000/record/`);
+
+      if (!response.ok) {
+        const message = `An error occurred: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
+
+      const records = await response.json();
+      setRecords(records);
+    }
+
+    getRecords();
+
+    return;
+  }, [records.length]);
+
+  //method deletes record
+  async function deleteRecord(id) {
+    await fetch(`http://localhost:5000/${id}`, {
+      method: "DELETE",
+    });
+
+    const newRecords = records.filter((el) => el._id !== id);
+    setRecords(newRecords);
+  }
+
+  //method maps records on a table
+  function recordList() {
+    return records.map((record) => {
+      return (
+        <Record
+          record={record}
+          deleteRecord={() => deleteRecord(record_id)}
+          key={record_id}
+        />
+      );
+    });
+  }
+  //displays table with records
+  return (
+    <div>
+      <h3>Troop 443 Record List</h3>
+      <table className="table table-striped" style={{ marginTop: 20 }}>
+        <thread>
+          <tr>
+            <th>Girl's Name</th>
+            <th>Contact</th>
+            <th>Level</th>
+            <th>Action</th>
+          </tr>
+        </thread>
+        <tbody>{recordList()}</tbody>
+      </table>
+    </div>
+  );
+}
